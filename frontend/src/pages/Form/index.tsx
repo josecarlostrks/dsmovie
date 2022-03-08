51266facb572a8febb1e693cef2895c41595e227
José Carlos <carlos.rodrigues@academico.ifpb.edu.br>
2022-03-07 21:16:05 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,8 @@
+function Form(){
+	return(
+		<h1>Página do formulário</h1>
+	);
+
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+function Listing(){
+	return(
+		<h1>Página de lsitagem</h1>
+	);
+}
+
+export default Listing;
